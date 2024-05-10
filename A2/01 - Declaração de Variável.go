b347b0e8ead5559b8a58deae34cbca082ed34056
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-05-10 20:27:40 -0300

    Add: Declaração de variável

---
+++ A2/01 - Declaração de Variável.go
@@ -0,0 +1,24 @@
+package main
+
+const a = "constante"
+
+var b bool
+var c int
+var d string
+
+var (
+	e int
+	f string
+)
+
+func main() {
+	var a string = "b"
+	b = true
+	println(d)
+}
+
+func main2() {
+	a := "b"
+	b := a
+	print(b)
+}

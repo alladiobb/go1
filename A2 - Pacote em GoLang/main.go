16129f5437f505566f7931aed63eb877f3ed76f4
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-05-19 15:29:34 -0300

    Add: Pacote em Golang

---
+++ A2 - Pacote em GoLang/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	// "gogo/matematica"
+
+	"github.com/google/uuid"
+)
+
+func main() {
+
+	// s := matematica.Soma(10, 20)
+
+	// fmt.Println("Resultado: %v", s)
+	// fmt.Println("Valor: %v", matematica.A)
+	fmt.Println(uuid.New())
+}

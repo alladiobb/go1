b2aec4d56847897752c1146b38a1a833c7f45016
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-05-19 20:35:23 -0300

    Add: Condicionais (IF and SWITCH)

---
+++ A1/20 - condicionais.go
@@ -0,0 +1,25 @@
+package main
+
+func main() {
+	a := 5
+	b := 6
+	c := 3
+
+	switch a {
+	case a:
+		println("a")
+	case b:
+		println("b")
+	case c:
+		println("c")
+	default:
+		println("default")
+	}
+
+	// if a > b && c > a {
+	// 	println("A")
+	// } else {
+	// 	println("B")
+	// }
+
+}

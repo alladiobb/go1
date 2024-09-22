72e5806fedf69623346db76088aeaa061005e9e6
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-09-22 19:59:30 -0300

    Add: Timeout

---
+++ A5.3 - HTTPCLIENT/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func main() {
+
+	client := http.Client{Timeout: time.Second}
+
+	respose, error := client.Get("http://google.com")
+
+	if error != nil {
+		panic(error)
+	}
+
+	defer respose.Body.Close()
+	body, error := ioutil.ReadAll(respose.Body)
+
+	if error != nil {
+		panic(error)
+	}
+	println(string(body))
+
+}

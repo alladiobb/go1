039bbdfbf508b1d3c8bde741639f22fe3cc27489
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-10-03 18:20:48 -0300

    Implement

--- A5.3 - HTTPCLIENT/03 - custom post.go
+++ A5.3 - HTTPCLIENT/03 - custom post.go
@@ -1,35 +1,32 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"io/ioutil"
 	"net/http"
-	"os"
 )
 
 func main() {
 
 	client := http.Client{}
-	request, error := http.NewRequest("GET", "http://google.com")
+	request, error := http.NewRequest("GET", "http://google.com", nil)
 
 	if error != nil {
 		panic(error)
 	}
 
-	request.Header.Set("Accept","application/json")
-	response , error := client.Do(request)
+	request.Header.Set("Accept", "application/json")
+	response, error := client.Do(request)
 	if error != nil {
 		panic(error)
 	}
 
 	defer response.Body.Close()
 	body, error := ioutil.ReadAll(response.Body)
-	
-	error != nil {
-		panic(errerror)
+
+	if error != nil {
+		panic(error)
 	}
 
 	println(string(body))
-	
+
 }

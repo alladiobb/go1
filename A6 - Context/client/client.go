f042953201ccf396fb47b8599b2461403b7d3a7c
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-10-08 23:12:05 -0300

    Add: using context.WithTimeout

---
+++ A6 - Context/client/client.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	// ctxt := context.Background()
+	ctxt, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	request, error := http.NewRequestWithContext(ctxt, "GET", "http://localhost:8080", nil)
+	if error != nil {
+		panic(error)
+	}
+	response, error := http.DefaultClient.Do(request)
+	if error != nil {
+		panic(error)
+	}
+
+	defer response.Body.Close()
+	io.Copy(os.Stdout, response.Body)
+}

8cef326715599ce86f88ebd882537cc93f50a4bf
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-10-19 10:40:22 -0300

    Add: GORM list and using variables

--- A7 - DB/ORM/main.go
+++ A7 - DB/ORM/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 
@@ -23,16 +25,30 @@ func main() {
 
 	db.AutoMigrate(&Product2{})
 
-	db.Create(&Product2{
-		Name:  "Notebook",
-		Price: 1000.00,
-	})
+	// db.Create(&Product2{
+	// 	Name:  "Notebook",
+	// 	Price: 1000.00,
+	// })
 
-	products := []Product2{
-		{Name: "Notebook", Price: 1000},
-		{Name: "Notebook2", Price: 2000},
-		{Name: "Notebook3", Price: 3000},
-	}
+	// products := []Product2{
+	// 	{Name: "Notebook", Price: 1000},
+	// 	{Name: "Notebook2", Price: 2000},
+	// 	{Name: "Notebook3", Price: 3000},
+	// }
 
-	db.Create(&products)
+	// db.Create(&products)
+
+	//Select por ID
+	// var product Product2
+	// db.First(&product, 5)
+	// fmt.Println(product)
+
+	//Select por variavel
+	var product2 Product2
+	db.First(&product2, "name = ?", "notebook3")
+	fmt.Println(product2)
+}
+
+func selectProduct() {
+
 }
